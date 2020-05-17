8061b66ab05cf64d17c7e2dd20b3e6e35c189455
BeomSeok <nbs0105@gmail.com>
2020-05-17 16:15:49 +0900

    Fix Practice2
    Fix the code

--- sumNumber/sumNumber.dart
+++ sumNumber/sumNumber.dart
@@ -1,8 +1,8 @@
- int sumNumbers(int n,int n2){
-    
-    return n+n2; 
+int sumNumbers(int n, int n2) {
+  return n + n2;
 }
 
-main(){
- print(sumNumbers(3, 4));
+final sumNumber = (int n, int n2) => n + n2;
+main() {
+  print(sumNumbers(3, 4));
 }
