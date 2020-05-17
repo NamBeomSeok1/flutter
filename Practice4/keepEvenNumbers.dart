2ac8109d38f484fdf98186902cf7045380a2a9f1
BeomSeok <nbs0105@gmail.com>
2020-05-17 17:18:31 +0900

    Fix Prctice4

--- Practice4/keepEvenNumbers.dart
+++ Practice4/keepEvenNumbers.dart
@@ -1,11 +1,9 @@
-Iterable<int> keepEvenNumbers(Iterable<int> Ary) {
-  var evenAry = Ary.where((Ary) => Ary.isEven);
-  for (final a in evenAry) {
-    print("$a is Even");
-  }
-  return evenAry;
+Iterable<int> keepEvenNumbers(Iterable<int> numbers) {
+  final evenNumbers = numbers.where((numbers) => numbers.isEven);
+
+  return evenNumbers;
 }
 
 main() {
-  keepEvenNumbers([1, 4, 5, 7, 8]);
+  print(keepEvenNumbers([1, 4, 5, 7, 8]));
 }
